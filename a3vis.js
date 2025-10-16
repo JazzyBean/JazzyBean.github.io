866f374da0757a3a94bc1ddeed5d3c2f8f285fb3
Jasmine Bamba <jasmine_bamba@sfu.ca>
2025-10-15 18:52:56 -0700

    fixed the js code and removed comments
    fixed the js code and removed comments

--- a3vis.js
+++ a3vis.js
@@ -61,103 +61,20 @@ fetchData().then(async (data) => {
   .title('Sales in Japan over time')
   .toSpec()
 
-  render("view", vlSpec);
-  render("view2", vlSpec2);
-  render("view3", vlSpec3);
-  render("view4", vlSpec4);
+  vegaEmbed("view", vlSpec);
+  vegaEmbed("view2", vlSpec2);
+  vegaEmbed("view3", vlSpec3);
+  vegaEmbed("view4", vlSpec4);
 
 });
 
 
+// async function render(viewID, spec) {
+//   const result = await vegaEmbed(viewID, spec);
+//   result.view.run();
+// }
+
 async function render(viewID, spec) {
-  const result = await vegaEmbed(viewID, spec);
+  const result = await vegaEmbed(`#${viewID}`, spec);
   result.view.run();
 }
-
-
-
-// function renderViz1(data){
-//     const spec = {
-//         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-//         "data": { "values": data },
-//         "mark": "circle",
-//         "encoding": {
-//             "x": { "field": "Genre", "type": "nominal", "title": "Genre" },
-//             "y": { "aggregate": "sum", "field": "Global_Sales", "type": "quantitative", "title": "Total Global Sales (millions)" },
-//             "color": { "field": "Platform", "type": "nominal", "title": "Platform" },
-//             "tooltip": [
-//                 { "field": "Genre", "type": "nominal" },
-//                 { "field": "Platform", "type": "nominal" },
-//                 { "field": "Global_Sales", "type": "quantitative" }
-//             ]
-//         },
-//         "width": 1000,
-//         "height": 400,
-//         "title": "Global Sales by Genre and Platform"
-//     };
-//     vegaEmbed('#viz1', spec);
-// }
-
-// function renderViz2(data){
-//     const spec = {
-//         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-//         "data": { "values": data },
-//         "mark": "tick",
-//         "encoding": {
-//             "x": { "field": "Year", "type": "temporal", "title": "Year" },
-//             "y": { "aggregate": "sum", "field": "Global_Sales", "type": "quantitative", "title": "Total Global Sales (millions)" },
-//             "color": { "field": "Platform", "type": "nominal", "title": "Platform" },
-//             "detail": { "field": "Genre", "type": "nominal" },       
-//             "tooltip": [
-//                 { "field": "Year", "type": "temporal" },
-//                 { "field": "Platform", "type": "nominal" },
-//                 { "field": "Genre", "type": "nominal" },
-//                 { "field": "Global_Sales", "type": "quantitative" }
-//             ]
-//         },
-//         "width": 1000,
-//         "height": 400,
-//         "title": "Sales Over Time by Platform and Genre"
-//     };
-//     vegaEmbed('#viz2', spec);
-// }
-
-// function renderViz3(data){
-//     const spec = {
-//         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-//         "data": { "values": data },
-//         "mark": "bar",
-        
-//         "encoding": {
-//             "x": { "field": "Platform", "type": "nominal" },
-//             "y": { "aggregate": "sum", "field": "Sales", "type": "quantitative", "title": "Total Sales (millions)" },
-//             "color": { "field": "Region", "type": "nominal", "title": "Region" },
-//             "tooltip": [
-//                 { "field": "Platform", "type": "temporal" },
-//                 { "field": "Region", "type": "nominal" },
-//                 { "field": "Sales", "type": "quantitative" }
-//             ]
-//         },
-//         "width": 1000,
-//         "height": 400,
-//         "title": "Regional Sales by Platform"
-//     };
-//     vegaEmbed('#viz3', spec);
-// }
-
-// // vl.markBar()
-// //   .data(videoGamesData)
-// //   .transform(
-// //     vl.fold(['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales'])
-// //       .as(['Region', 'Sales'])
-// //   )
-// //   .encode(
-// //     vl.x().fieldN('Platform').title('Platform'),
-// //     vl.y().sum('Sales').title('Total Sales (millions)'),
-// //     vl.color().fieldN('Region').title('Region'),
-// //     vl.tooltip(['Platform', 'Region', 'Sales'])
-// //   )
-// //   .width(1000)
-// //   .height(400)
-// //   .title('Regional Sales by Platform')
-// //   .render()
